e49cd03a812e417786e48af04fa8dc2244054324
Andrew Chan <andrewhongchan@gmail.com>
2024-11-25 13:43:22 -0500

    Add basic graph functions (NOT TESTED YET)

--- Graph.cpp
+++ Graph.cpp
@@ -1,4 +1,6 @@
 #include "Graph.h"
+#include <iostream>
+using namespace std;
 
 Graph::~Graph() {
     
@@ -22,8 +24,84 @@ Node* Graph::searchForNode(string id) {
     return nullptr;
 }
 
-void Graph::addRelationship(string sourceId, string label, string destinationId, double weight);
-void Graph::addEntity(string id, string name, string type);
-void Graph::printAdjacent(string id);
-void Graph::deleteEntity(string id);
+void Graph::addRelationship(string sourceId, string label, string destinationId, double weight) {
+    Node* sourceNode = searchForNode(sourceId);
+    Node* destinationNode = searchForNode(destinationId);
+
+    if (sourceNode == nullptr || destinationNode == nullptr) {
+        cout << "failure" << endl;
+        return;
+    }
+
+    sourceNode->addAdjacentNode(destinationNode, label, weight);
+    destinationNode->addAdjacentNode(sourceNode, label, weight);
+    cout << "success" << endl;
+}
+
+void Graph::addEntity(string id, string name, string type) {
+    Node* node = searchForNode(id);
+
+    if (node) {
+        node->setName(name);
+        node->setLabel(type);
+        cout << "success" << endl;
+        return;
+    }
+
+    node = new Node(type, id, name);
+    entities.push_back(node);
+    cout << "success" << endl;
+
+
+}
+void Graph::printAdjacent(string id) {
+    Node* node = searchForNode(id);
+
+    if (node == nullptr) {
+        cout << "failure" << endl;
+        return;
+    }
+
+    if (node->getAdjacentNodes().size() == 0) {
+        cout << endl;
+        return;
+    }
+
+    for (int i = 0; i < node->getAdjacentNodes().size(); i++) {
+        cout << get<0>(node->getAdjacentNodes()[i])->getId() << " ";
+    }
+
+    cout << endl;
+}
+
+void Graph::deleteEntity(string id) {
+
+    Node* node = searchForNode(id); 
+
+    if (node == nullptr) {
+        cout << "failure" << endl;
+        return;
+    }
+
+    for (int i = 0; i < entities.size(); i++) {
+
+        if (entities[i] != node) {
+            entities[i]->removeAdjacentNode(node);
+        }
+    }
+
+    int itAdd = 0;
+    for (int i = 0; i < entities.size(); i++) {
+        if (entities[i] == node) {
+            itAdd = i;
+            break;
+        }
+    }
+
+    delete node;
+
+    entities.erase(entities.begin() + itAdd);
+}
+
+
         
