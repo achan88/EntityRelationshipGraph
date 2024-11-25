d4d4d0c364f178bc92d00a77f86a639064c7230a
Andrew Chan <andrewhongchan@gmail.com>
2024-11-24 22:17:32 -0500

    Added valid id checking + searching if a node exists in the graph

--- Graph.cpp
+++ Graph.cpp
@@ -1,13 +1,26 @@
 #include "Graph.h"
 
-~Graph::Graph() {
-    for (int i = 0; i < vertices)
+Graph::~Graph() {
+    
 }
-bool Graph::isValidId(string id);
+bool Graph::isValidId(string id) {
+    for (int i = 0; i < id.length(); i++) {
+        if (!((id[i] >= 'A' && id[i] <= 'Z') || (id[i] >= 'a' && id[i] <= 'z') || (id[i] >= '0' && id[i] <= '9'))) {
+            return false; 
+        }
+    }
+    return true;
+}
 
+Node* Graph::searchForNode(string id) {
 
-
-Node* Graph::searchForNode(string id);
+    for (int i = 0; i < entities.size(); i++) {
+        if (entities[i]->getId() == id) {
+            return entities[i];
+        }
+    }
+    return nullptr;
+}
 
 void Graph::addRelationship(string sourceId, string label, string destinationId, double weight);
 void Graph::addEntity(string id, string name, string type);
