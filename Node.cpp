c3d203d60baff12b999a3cebc260c40b4f2b62cb
Andrew Chan <andrewhongchan@gmail.com>
2024-11-24 22:01:07 -0500

    Add and remove nodes from adjacency list works

--- Node.cpp
+++ Node.cpp
@@ -27,3 +27,41 @@ void Node::setName(string newName) {
     name = newName;
 }
 
+vector<tuple<Node*, string, double>>& Node::getAdjacentNodes() {
+    return adjacentNodes;
+}
+
+void Node::addAdjacentNode(Node* node, string label, double weight) {
+            
+    for (int i = 0; i < adjacentNodes.size(); i++) {
+        if (get<0>(adjacentNodes[i]) == node) {
+            get<1>(adjacentNodes[i]) = label;
+            get<2>(adjacentNodes[i]) = weight;
+            return;
+        }
+    }
+
+    tuple<Node*, string, double> myTuple = make_tuple(node, label, weight);
+    adjacentNodes.push_back(myTuple);
+}
+
+void Node::removeAdjacentNode(Node* node) {
+
+    for (int i = 0; i < adjacentNodes.size(); i++) {
+        if (get<0>(adjacentNodes[i]) == node) {
+            adjacentNodes.erase(adjacentNodes.begin() + i);
+            return;
+        }
+    }
+
+}
+bool Node::isAdjacentNode(Node* node) {
+    for (int i = 0; i < adjacentNodes.size(); i++) {
+        if (get<0>(adjacentNodes[i]) == node) {
+            return true;
+        }
+    }
+    return false;
+}
+
+
