f9d0038062c3a88d445a880ee3109cb04f3d2380
Andrew Chan <andrewhongchan@gmail.com>
2024-11-29 19:46:30 -0500

    Change illegal exception to be a header file

--- illegal_exception.cpp
+++
@@ -1,4 +0,0 @@
-class illegal_exception {
-public:
-    illegal_exception() = default;
-};
---
+++ illegal_exception.h
@@ -0,0 +1,6 @@
+class illegal_exception {
+
+    public:
+        illegal_exception() = default;
+        
+};
