711fe4f9206655578147af980216a4b0529323fc
Andrew Chan <andrewhongchan@gmail.com>
2024-11-29 20:09:59 -0500

    Add all commands to main

--- main.cpp
+++ main.cpp
@@ -20,6 +20,40 @@ int main() {
 
             cin >> sourceId >> label >> destinationId >> weight;
             graph.addRelationship(sourceId, label, destinationId, weight);
+        } else if (command == "ENTITY") {
+            string id;
+            string name;
+            string type;
+
+            cin >> id >> name >> type;
+
+            graph.addEntity(id, name, type);
+        } else if (command == "PRINT") {
+            string id;
+            cin >> id;
+
+            graph.printAdjacent(id);
+        } else if (command == "DELETE") {
+            string id;
+            cin >> id;
+
+            graph.deleteEntity(id);
+        } else if (command == "PATH") {
+            string id1;
+            string id2;
+            cin >> id1 >> id2;
+
+            graph.path(id1, id2);
+        } else if (command == "HIGHEST") {
+
+            cout << "TODO" << endl;
+        } else if (command == "FINDALL") {
+            string type;
+            string string;
+
+            graph.findAll(type, string);
+        } else if (command == "EXIT") {
+            break;
         }
     }
 }
